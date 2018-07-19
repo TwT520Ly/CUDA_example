19510f203d74ee3508d29625b89ed110ff98a4b2
twt <tan.wt.lucky@gmail.com>
2018-07-19 12:13:20 +0800

    test time

--- cuda_demo_001/src/main.cpp
+++ cuda_demo_001/src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "matrix.cuh"
+#include <ctime>
+#include <typeinfo>
 
-#define MAX_SIZE 10005
+#define MAX_SIZE 1000005
 using namespace std;
 
 int data[MAX_SIZE];
-const int DATA_SIZE = 100;
+const int DATA_SIZE = 1000000;
 
 void generateNumbers(int* number, int size) {
     for (int i=0; i<size; i++) {
@@ -26,16 +28,22 @@ int main() {
 
     // 使用GPU进行测试
     int sum_gpu;
+    clock_t start_gpu = clock();
     sum_gpu = sumOfSquares(data, DATA_SIZE);
-
+    clock_t end_gpu = clock();
     std::cout << "GPU sum: " << sum_gpu << std::endl;
+    std::cout << (end_gpu - start_gpu) << std::endl;
 
     // 使用CPU进行测试
     int sum_cpu = 0;
+    clock_t start_cpu = clock();
     for (int i=0; i<DATA_SIZE; i++) {
         sum_cpu += data[i] * data[i] * data[i];
     }
+    clock_t end_cpu = clock();
     std::cout << "CPU sum: "<< sum_cpu << std::endl;
+    std::cout << (end_cpu - start_cpu) << std::endl;
+
     return 0;
 }
 
